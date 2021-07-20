53f05b68ccd662b4f4e1054706b6bdc846550574
Saoud <saoud@saoud.dev>
2021-07-19 20:36:01 -0700

    fixing security issues in package-lock.json
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -12,7 +12,10 @@ describe('Coin', () => {
   })
 
   test('should correctly return coin', () => {
-    console.log(testCoin.coinCounter(4.99))
+    expect(testCoin.coinCounter(0)).toEqual({"dime": 0, "nickle": 0, "penny": 0, "quarter": 0});
+  });
+
+  test('should correctly return coin', () => {
     expect(testCoin.coinCounter(4.99)).toEqual({"dime": 24, "nickle": 0, "penny": 4, "quarter": 19});
   });
 });
